f7a3fa9211f23f9e4baab9e8b98c5c5bdaaa99c4
ifat <i>
2024-07-30 10:30:36 +0100

    introduced components and load data with JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      
-      <h1 className='text-3xl font-bold'>Blogs Hub</h1>
-      
+      <Header></Header>
+      <Blogs></Blogs>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Blogs Hub</h1>
+    </div>
+  );
+};
+
+export default Header;
