6431cc6242988eaf2eea00caaaec866914a62d6a
ifat <i>
2024-08-01 00:52:11 +0100

    display blog data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,43 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    reading_time,
+    posted_date,
+    hashtags,
+  } = blog;
+
+  return (
+    <div>
+      <img src={cover} alt={`Cover Picture of ${title}`} />
+      <div className="flex justify-between">
+        <div className="flex gap-4">
+          <img className="w-14" src={author_img} alt="" />
+          <div>
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl ">{title}</h2>
+      <p>
+        {
+            hashtags.map(hash => <span><a>#{hash}</a></span>)
+        }
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-  blog: PropTypes.object.isRequired
+  blog: PropTypes.object.isRequired,
 };
 
 export default Blog;
