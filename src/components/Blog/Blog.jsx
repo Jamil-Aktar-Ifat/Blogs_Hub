ebfd6e3008e66ef2e281e7b72b7ac275bc9a8e60
ifat <i>
2024-08-01 01:53:07 +0100

    handled bookmark state and displayed bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -29,7 +29,10 @@ const Blog = ({ blog, handleAddToBookmark }) => {
         </div>
         <div className="flex items-center">
           <span>{reading_time} min read</span>
-          <button onClick={handleAddToBookmark} className="ml-2 text-2xl">
+          <button
+            onClick={() => handleAddToBookmark(blog)}
+            className="ml-2 text-2xl"
+          >
             <CiBookmark />
           </button>
         </div>
@@ -48,6 +51,7 @@ const Blog = ({ blog, handleAddToBookmark }) => {
 
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
+  handleAddToBookmark: PropTypes.func
 };
 
 export default Blog;
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,18 @@
-const Bookmarks = () => {
+import PropTypes from "prop-types";
+import Bookmark from "../../Bookmark/Bookmark";
+
+
+const Bookmarks = ({ bookmarks }) => {
   return (
-    <div className="md: w-1/3">
-      <h1 className="text-2xl">BookMarks</h1>
+    <div className="md: w-1/3 bg-gray-300 ml-4 mt-2 pt-4 rounded-xl">
+      <h1 className="text-4xl text-center">BookMarked Blogs: {bookmarks.length}</h1>
+      {bookmarks.map((bookmark) => (
+        <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>
+      ))}
     </div>
   );
 };
-
+Bookmarks.propTypes = {
+  bookmarks: PropTypes.array,
+};
 export default Bookmarks;
